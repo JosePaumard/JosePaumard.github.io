62be7e5903d49c9532069ca099862dbb14fd064d
José Paumard <Jose.Paumard@gmail.com>
2017-03-11 11:23:10 +0100

    First red test

---
+++ src/test/java/org/paumard/katas/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 José Paumard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.paumard.katas.fizzbuzz;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+/**
+ * Created by José
+ */
+public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_fizzBuzzing_1() {
+
+        // Given
+        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+        int input = 1;
+        String expectedOutput = "1";
+
+        // When
+        String output = fizzBuzzer.fizzBuzz(input);
+
+        // Then
+        assertThat(output).isEqualTo(expectedOutput);
+    }
+}
