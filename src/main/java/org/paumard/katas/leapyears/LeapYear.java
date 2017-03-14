5f1b495791ae26d0f45b8706c8841d698c736514
José <Jose.Paumard@gmail.com>
2017-03-14 14:17:23 +0100

    Made the test green in the most simple (dumb) way

--- src/main/java/org/paumard/katas/leapyears/LeapYear.java
+++ src/main/java/org/paumard/katas/leapyears/LeapYear.java
@@ -22,6 +22,9 @@ package org.paumard.katas.leapyears;
 public class LeapYear {
 
     public boolean isLeapYear(int year) {
+        if (year == 1900) {
+            return false;
+        }
         return isTypicalLeapYear(year);
     }
 
