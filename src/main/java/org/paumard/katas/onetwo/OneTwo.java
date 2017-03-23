075c215003955e8bd7830a5f37156d9ed91a81e7
José Paumard <Jose.Paumard@gmail.com>
2017-03-23 14:00:43 +0100

    Added en enumerated value to make the test pass

--- src/main/java/org/paumard/katas/onetwo/OneTwo.java
+++ src/main/java/org/paumard/katas/onetwo/OneTwo.java
@@ -31,7 +31,8 @@ public class OneTwo {
         FIVE("5", "five"),
         SIX("6", "six"),
         SEVEN("7", "seven"),
-        EIGHT("8", "eight");
+        EIGHT("8", "eight"),
+        NINE("9", "nine");
 
         private final String number;
         private final String name;
