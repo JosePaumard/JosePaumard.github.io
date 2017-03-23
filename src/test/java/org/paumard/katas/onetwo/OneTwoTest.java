f2a0ca5a11b5d73420b025d7596358fd3042524b
José Paumard <Jose.Paumard@gmail.com>
2017-03-23 13:58:35 +0100

    Added a failing test for 8

--- src/test/java/org/paumard/katas/onetwo/OneTwoTest.java
+++ src/test/java/org/paumard/katas/onetwo/OneTwoTest.java
@@ -129,4 +129,19 @@ public class OneTwoTest {
         // Then
         assertThat(result).isEqualTo(expectedResult);
     }
+
+    @Test
+    public void should_return_one_two_for_8() {
+
+        // Given
+        OneTwo oneTwo = new OneTwo();
+        String input = "8";
+        String expectedResult = "one eight";
+
+        // When
+        String result = oneTwo.convertToNames(input);
+
+        // Then
+        assertThat(result).isEqualTo(expectedResult);
+    }
 }
