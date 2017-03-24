8d3e7fc24baafc46e820a2082c1235032da82556
José Paumard <Jose.Paumard@gmail.com>
2017-03-24 20:07:38 +0100

    First commit with first, non-compiling test. Also added the file with the palindromes

---
+++ java/test/src/org/paumard/katas/palindrome/PalindromeTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 José Paumard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.paumard.katas.palindrome;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+/**
+ * Created by José
+ */
+public class PalindromeTest {
+
+    @Test
+    public void should_return_true_for_empty_string() {
+
+        // Given
+        Palindrome palindrome = new Palindrome();
+        String input = "";
+        boolean expectedResult = false;
+
+        // When
+        boolean result = palindrome.isPalindrome(input);
+
+        // Then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
