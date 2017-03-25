e0f2cd3a6d0db3bae2460ee3966de7a5f1bf534e
José Paumard <Jose.Paumard@gmail.com>
2017-03-25 07:42:54 +0100

    Added a failing test

--- java/test/src/org/paumard/katas/palindrome/PalindromeTest.java
+++ java/test/src/org/paumard/katas/palindrome/PalindromeTest.java
@@ -99,4 +99,19 @@ public class PalindromeTest {
         // Then
         assertThat(result).isEqualTo(expectedResult);
     }
+
+    @Test
+    public void should_return_true_for_a_many_chars_palindrome() {
+
+        // Given
+        Palindrome palindrome = new Palindrome();
+        String input = "ABCDEFGHIIHGFEDBC";
+        boolean expectedResult =  true;
+
+        // When
+        boolean result = palindrome.isPalindrome(input);
+
+        // Then
+        assertThat(result).isEqualTo(expectedResult);
+    }
 }
