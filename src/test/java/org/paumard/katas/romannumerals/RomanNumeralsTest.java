063c2b18949433b167af8a7897cf3060b7531ab8
José Paumard <Jose.Paumard@gmail.com>
2017-04-20 18:49:14 +0200

    Added a failing test

--- src/test/java/org/paumard/katas/romannumerals/RomanNumeralsTest.java
+++ src/test/java/org/paumard/katas/romannumerals/RomanNumeralsTest.java
@@ -473,4 +473,19 @@ public class RomanNumeralsTest {
         // Then
         Assertions.assertThat(output).isEqualTo(expectedOutput);
     }
+
+    @Test
+    public void should_return_12_for_XII() {
+
+        // Given
+        String input = "XII";
+        int expectedOutput = 12;
+        RomanNumerals romanNumerals = new RomanNumerals();
+
+        // When
+        int output = romanNumerals.toArabics(input);
+
+        // Then
+        Assertions.assertThat(output).isEqualTo(expectedOutput);
+    }
 }
