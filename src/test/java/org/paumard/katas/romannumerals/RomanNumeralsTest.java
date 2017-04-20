30239e06a1fec02d4a17fead6df9e45e8c08280c
José Paumard <Jose.Paumard@gmail.com>
2017-04-20 15:25:13 +0200

    Added a failing test

--- src/test/java/org/paumard/katas/romannumerals/RomanNumeralsTest.java
+++ src/test/java/org/paumard/katas/romannumerals/RomanNumeralsTest.java
@@ -83,4 +83,19 @@ public class RomanNumeralsTest {
         // Then
         Assertions.assertThat(output).isEqualTo(expectedOutput);
     }
+
+    @Test
+    public void should_return_V_for_5() {
+
+        // Given
+        int input = 5;
+        String expectedOutput = "V";
+        RomanNumerals romanNumerals = new RomanNumerals();
+
+        // When
+        String output = romanNumerals.toRoman(input);
+
+        // Then
+        Assertions.assertThat(output).isEqualTo(expectedOutput);
+    }
 }
