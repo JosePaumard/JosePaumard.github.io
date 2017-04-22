84f838fbe1c00438f894400c837aa81bfc00d9e7
José <Jose.Paumard@gmail.com>
2017-04-22 14:45:29 +0200

    Created the test class with a first test, that does not compile

---
+++ src/test/java/org/paumard/katas/fizzbuzzwoof/FizzzBuzzWoofTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2017 José Paumard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.paumard.katas.fizzbuzzwoof;
+
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+/**
+ * Created by José
+ */
+public class FizzzBuzzWoofTest {
+
+    @Test
+    public void should_return_1_for_1() {
+
+        // Given
+        int input = 1;
+        String expectedOuput = "1";
+        FizzBuzzWoof fizzBuzzWoof = new FizzzBuzzWoof();
+
+        // When
+        String output = fizzBuzzWoof.convert(input);
+
+        // Then
+        Assertions.assertThat(output).isEqualTo(expectedOuput);
+    }
+}
