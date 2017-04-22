618c5e37c0743df8b9be944bdb15ea8b97a3a232
José <Jose.Paumard@gmail.com>
2017-04-22 16:23:50 +0200

    Added a test that happens to pass

--- src/test/java/org/paumard/katas/fizzbuzzwoof/FizzzBuzzWoofTest.java
+++ src/test/java/org/paumard/katas/fizzbuzzwoof/FizzzBuzzWoofTest.java
@@ -173,4 +173,19 @@ public class FizzzBuzzWoofTest {
         // Then
         Assertions.assertThat(output).isEqualTo(expectedOuput);
     }
+
+    @Test
+    public void should_return_BuzzWoof_for_140() {
+
+        // Given
+        int input = 140;
+        String expectedOuput = "BuzzWoof";
+        FizzBuzzWoof fizzBuzzWoof = new FizzBuzzWoof();
+
+        // When
+        String output = fizzBuzzWoof.convert(input);
+
+        // Then
+        Assertions.assertThat(output).isEqualTo(expectedOuput);
+    }
 }
