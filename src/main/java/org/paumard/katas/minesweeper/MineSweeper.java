acdfdc30f88c50571592c11b170b5cc211169509
José <Jose.Paumard@gmail.com>
2018-01-14 21:56:28 +0100

    Added a first very simple and failing test

---
+++ src/main/java/org/paumard/katas/minesweeper/MineSweeper.java
@@ -0,0 +1,12 @@
+package org.paumard.katas.minesweeper;
+
+public class MineSweeper {
+
+    public void init(String inputField) {
+
+    }
+
+    public String produceHintField() {
+        return null;
+    }
+}
--- src/test/java/org/paumard/katas/minesweeper/MineSweeperStepDefs.java
+++ src/test/java/org/paumard/katas/minesweeper/MineSweeperStepDefs.java
@@ -1,24 +1,30 @@
 package org.paumard.katas.minesweeper;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+
 public class MineSweeperStepDefs {
+
+    private MineSweeper mineSweeper;
+    private String hintField;
 
     @Given("^The following field$")
     public void the_following_field(String inputField) throws Throwable {
-        throw new PendingException();
+        this.mineSweeper = new MineSweeper();
+        this.mineSweeper.init(inputField);
     }
 
     @When("^The resulting field is computed$")
     public void the_resulting_field_is_computed() throws Throwable {
-        throw new PendingException();
+        this.hintField = this.mineSweeper.produceHintField();
     }
 
     @Then("^The result is the following$")
     public void the_result_is_the_following(String expectedResult) throws Throwable {
-        throw new PendingException();
+        assertThat(this.hintField).isEqualTo(expectedResult);
     }
 }
