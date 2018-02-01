64a5ce3bb2dafaae04f5dedebf68b8abaa56a452
José <Jose.Paumard@gmail.com>
2018-02-01 11:40:16 +0100

    Added the class and made the test run, and fail

---
+++ src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
@@ -0,0 +1,9 @@
+package org.paumard.katas.phonenumbers;
+
+import java.util.List;
+
+public class PhoneNumbers {
+    public boolean isConsistent(List<String> phoneNumbersList) {
+        return false;
+    }
+}
--- src/test/java/org/paumard/katas/phonenumbers/PhoneNumbersStepDefs.java
+++ src/test/java/org/paumard/katas/phonenumbers/PhoneNumbersStepDefs.java
@@ -4,26 +4,33 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.assertj.core.api.Assertions;
 
 import java.util.List;
 
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
 public class PhoneNumbersStepDefs {
 
+    private List<String> phoneNumbersList;
+    private boolean isConsistent;
+
     @Given("^The following list$")
-    public void the_following_list(List<String> phoneNumbers) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_following_list(List<String> phoneNumbersList) throws Throwable {
+
+        this.phoneNumbersList = phoneNumbersList;
     }
 
     @When("^The list is checked for consistency$")
     public void the_list_is_checked_for_consistency() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        PhoneNumbers phoneNumbers = new PhoneNumbers();
+        isConsistent = phoneNumbers.isConsistent(phoneNumbersList);
     }
 
     @Then("^The list is consistent$")
     public void the_list_is_consistent() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        assertThat(isConsistent).isTrue();
     }
 }
