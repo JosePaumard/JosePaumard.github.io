cd1356b4b8d6a9bcc40b9c151e57662e72745b68
José <Jose.Paumard@gmail.com>
2018-02-01 10:55:32 +0100

    Made the test pass in a simple, dumb way

--- src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
+++ src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
@@ -8,9 +8,16 @@ public class PhoneNumbers {
         if (phoneNumbers.size() == 1) {
             return true;
         }
-        String firstPhoneNumber = phoneNumbers.get(0);
-        String secondPhoneNumber = phoneNumbers.get(1);
-        return !secondPhoneNumber.startsWith(firstPhoneNumber) &&
-               !firstPhoneNumber.startsWith(secondPhoneNumber);
+
+        String prefix = phoneNumbers.get(0);
+        for (int index = 1; index < phoneNumbers.size(); index++) {
+            String phoneNumber = phoneNumbers.get(index);
+            if (phoneNumber.startsWith(prefix)) {
+                return false;
+            } else if (prefix.startsWith(phoneNumber)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
