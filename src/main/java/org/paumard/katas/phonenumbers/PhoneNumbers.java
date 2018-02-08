e2a335402413abff759256728d0d0e6ba8f7955b
José <Jose.Paumard@gmail.com>
2018-02-08 13:40:52 +0100

    Some more refactoring

--- src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
+++ src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
@@ -4,21 +4,22 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PhoneNumbers {
     public boolean isConsistent(List<String> phoneNumbersList) {
 
         Set<String> phoneNumberSet = new HashSet<>();
         for (String phoneNumber : phoneNumbersList) {
-            if (
-            IntStream.rangeClosed(1, phoneNumber.length())
-                    .mapToObj(index -> phoneNumber.substring(0, index))
-                    .anyMatch(phoneNumberSet::contains)
-                    ) {
+            if (streamOfPrefixes(phoneNumber).anyMatch(phoneNumberSet::contains)) {
                 return false;
             }
             phoneNumberSet.add(phoneNumber);
         }
         return true;
+    }
+
+    static Stream<String> streamOfPrefixes(String string) {
+        return IntStream.rangeClosed(1, string.length()).mapToObj(index -> string.substring(0, index));
     }
 }
