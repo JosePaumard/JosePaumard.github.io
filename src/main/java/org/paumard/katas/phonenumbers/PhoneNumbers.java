965c017e025b3d1cc6749b81d78eafe8d1e8cdc1
José <Jose.Paumard@gmail.com>
2018-02-08 11:21:41 +0100

    First step to extend the fix, red tests!

--- src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
+++ src/main/java/org/paumard/katas/phonenumbers/PhoneNumbers.java
@@ -9,9 +9,11 @@ public class PhoneNumbers {
 
         Set<String> phoneNumberSet = new HashSet<>();
         for (String phoneNumber : phoneNumbersList) {
-            String phoneNumberPrefix = phoneNumber.substring(0, 1);
-            if (phoneNumberSet.contains(phoneNumberPrefix)) {
-                return false;
+            for (int index = 1; index < phoneNumber.length(); index++) {
+                String phoneNumberPrefix = phoneNumber.substring(0, index);
+                if (phoneNumberSet.contains(phoneNumberPrefix)) {
+                    return false;
+                }
             }
             phoneNumberSet.add(phoneNumber);
         }
