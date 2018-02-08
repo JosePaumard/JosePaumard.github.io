70c10fcf390f2c12c566b2d43314864bbb61fc07
José <Jose.Paumard@gmail.com>
2018-02-08 15:29:46 +0100

    Fixed the code to make the test pass

--- src/main/java/org/paumard/katas/wordwrap/WordWrapper.java
+++ src/main/java/org/paumard/katas/wordwrap/WordWrapper.java
@@ -1,8 +1,17 @@
 package org.paumard.katas.wordwrap;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class WordWrapper {
 
     public String wrap(int numberOfColumns, String line) {
-        return line;
+        if (line.length() < numberOfColumns) {
+            return line;
+        }
+        int index = line.indexOf(" ");
+        String firstLine = line.substring(0, index);
+        String secondLine = line.substring(index + 1);
+        return Stream.of(firstLine, secondLine).collect(Collectors.joining("\n"));
     }
 }
