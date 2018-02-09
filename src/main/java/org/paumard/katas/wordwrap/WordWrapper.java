e81cbff1ea19dbeb60e25a6ad034afeca7464abb
José <Jose.Paumard@gmail.com>
2018-02-09 16:12:28 +0100

    Refactored to remove the limit variable

--- src/main/java/org/paumard/katas/wordwrap/WordWrapper.java
+++ src/main/java/org/paumard/katas/wordwrap/WordWrapper.java
@@ -13,23 +13,24 @@ public class WordWrapper {
             List<String> lines = new ArrayList<>();
             String remainingLine = line;
             int limit = getLimit(remainingLine, numberOfColumns);
-            String nextSegment = getNextSegment(remainingLine, numberOfColumns, limit);
+            String nextSegment = getNextSegment(remainingLine, numberOfColumns);
             lines.add(nextSegment);
-            remainingLine = getRemainingLine(remainingLine, numberOfColumns, limit);
+            remainingLine = getRemainingLine(remainingLine, numberOfColumns);
 
             while (remainingLine.length() > numberOfColumns) {
 
                 limit = getLimit(remainingLine, numberOfColumns);
-                nextSegment = getNextSegment(remainingLine, numberOfColumns, limit);
+                nextSegment = getNextSegment(remainingLine, numberOfColumns);
                 lines.add(nextSegment);
-                remainingLine = getRemainingLine(remainingLine, numberOfColumns, limit);
+                remainingLine = getRemainingLine(remainingLine, numberOfColumns);
             }
             lines.add(remainingLine);
             return lines.stream().collect(Collectors.joining("\n"));
         }
     }
 
-    private String getRemainingLine(String remainingLine, int numberOfColumns, int limit) {
+    private String getRemainingLine(String remainingLine, int numberOfColumns) {
+        int limit = getLimit(remainingLine, numberOfColumns);
         String nextPart = remainingLine.substring(0, numberOfColumns);
         if (nextPart.contains(" ")) {
             remainingLine = remainingLine.substring(limit + 1);
@@ -39,7 +40,8 @@ public class WordWrapper {
         return remainingLine;
     }
 
-    private String getNextSegment(String remainingLine, int numberOfColumns, int limit) {
+    private String getNextSegment(String remainingLine, int numberOfColumns) {
+        int limit = getLimit(remainingLine, numberOfColumns);
         String nextPart = remainingLine.substring(0, numberOfColumns);
         if (nextPart.contains(" ")) {
             return nextPart.substring(0, limit);
