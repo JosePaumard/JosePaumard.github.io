7ca1b7c5e4c4501482ff33cc51ad74a740fde0d6
José <Jose.Paumard@gmail.com>
2018-02-09 12:09:02 +0100

    Refactored to make duplicated code appear

--- src/main/java/org/paumard/katas/wordwrap/WordWrapper.java
+++ src/main/java/org/paumard/katas/wordwrap/WordWrapper.java
@@ -1,36 +1,42 @@
 package org.paumard.katas.wordwrap;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WordWrapper {
 
     public String wrap(int numberOfColumns, String line) {
         if (line.length() <= numberOfColumns) {
             return line;
         } else {
-            String result = "";
-            String remainingLine;
-            String nextPart = line.substring(0, numberOfColumns);
+            List<String> lines = new ArrayList<>();
+            String remainingLine = line;
+            String nextPart = remainingLine;
+            nextPart = remainingLine.substring(0, numberOfColumns);
             if (nextPart.contains(" ")) {
                 int limit = nextPart.lastIndexOf(' ');
-                nextPart = line.substring(0, limit);
-                remainingLine = line.substring(limit + 1);
+                lines.add(nextPart.substring(0, limit));
+                remainingLine = remainingLine.substring(limit + 1);
             } else {
-                remainingLine = line.substring(numberOfColumns);
+                lines.add(nextPart);
+                remainingLine = remainingLine.substring(numberOfColumns);
             }
-            result = nextPart;
 
             while (remainingLine.length() >  numberOfColumns) {
 
                 nextPart = remainingLine.substring(0, numberOfColumns);
                 if (nextPart.contains(" ")) {
                     int limit = nextPart.lastIndexOf(' ');
-                    result += "\n" + nextPart.substring(0, limit);
+                    lines.add(nextPart.substring(0, limit));
                     remainingLine = remainingLine.substring(limit + 1);
                 } else {
-                    result += "\n" + nextPart;
+                    lines.add(nextPart);
                     remainingLine = remainingLine.substring(numberOfColumns);
                 }
             }
-            return result + "\n" + remainingLine;
+            lines.add(remainingLine);
+            return lines.stream().collect(Collectors.joining("\n"));
         }
     }
 }
